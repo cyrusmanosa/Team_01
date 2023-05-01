822744dc0acab994fcaaca90e8440e74f0576294
hikaridoll <hikabeeemu0112@icloud.com>
2023-05-01 12:25:26 +0900

    変更

--- next-app/src/pages/addComplete.js
+++ next-app/src/pages/addComplete.js
@@ -3,22 +3,26 @@ import style from "../styles/situation.module.css"
 export default function addComplete() {
     return (
       <>
-        <p className={style.title}>追加</p>
-        <p classNaem={style.text}>新社員の情報を教えてください。</p>
+      <div className={style.container}>
+        <div>
+          <h1 className={style.title}>追加</h1>
+        </div>
 
         <div className={style.inputform}>
+          <div  className={style.text}>
+            <h2> 社員 : 社員のID追加できました</h2>
+            <h2>ありがとうございました！</h2>
+          </div>
 
-        <h2 className="Stitle"> 社員：（社員のID）追加できました</h2>
-        <h2>ありがとうございました！</h2>
+          <div className={style.Cen}>
+              <a href="./completoin.js"><img src="./img/Cen2.png" width={100} /></a>
+          </div>
 
-        <div className={style.Cen}>
-            <a href="./menu.html"><img src="Cen2.png" width={100} /></a>
-        </div>
-
-        <div className={style.footerPart}>
+          <div className={style.footerPart}>
             <h3 className="footerWord">システムの状況:X</h3>
+          </div>
+        <hr className={style.hr}></hr>
         </div>
-        
         </div>
       </>
     );
