edd4151e4863a2393237f4094ca724648ebaf6ae
hikaridoll <hikabeeemu0112@icloud.com>
2023-05-01 10:15:43 +0900

    変更

--- next-app/src/pages/situation2.js
+++ next-app/src/pages/situation2.js
@@ -1,24 +1,26 @@
 import style from "../styles/situation.module.css";
 import movePage from "./_route.js";
-const situation3 = () => {
+const situation2 = () => {
     return (
         <div className={style.container}>
             <div>
                 <h1 className={style.title}>安否登録</h1>
-                <p>あなたの安否状況を教えてください。</p>
+                <p>怪我をしていますか？</p>
             </div>
 
             <form action="" method="post">
                 <div>
-                    <input className={style.safe} type="button" value="無事です" onClick={movePage("situation3")}></input>
+                    <input className={style.safe} type="button" value="怪我無し" onClick={movePage("situation3")}></input>
                 </div>
                 <div>
-                    <input className={style.dangerous} type="button" value="危険です" onClick={movePage("situation3")}></input>
+                    <input className={style.dangerous} type="button" value="怪我有り" onClick={movePage("situation3")}></input>
                 </div>
             </form>
 
             <div className={style.img}>
-                <a href="../pages/situation1.js"><img src="../img/Left.png" width="100px"></img></a>
+                <div>
+                    <a href="../pages/situation1.js"><img src="../img/Left.png" width="100px"></img></a>
+                </div>
                 <h3>システムの状況</h3>
                 <a href="../pages/situation3.js"><img src="../img/Right.png" width="100px"></img></a>
             </div>
