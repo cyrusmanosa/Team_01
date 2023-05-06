2605e180db5dc88693e37b0bafe52d4fc276a9bf
gongye16 <kinggongye@gmail.com>
2023-05-07 01:25:27 +0900

    レイアウト変更・ページ遷移改善しました

--- next-app/src/pages/add.js
+++ next-app/src/pages/add.js
@@ -1,34 +1,55 @@
-import style from "../styles/situation.module.css"
+import style from "../styles/situation.module.css";
+import movePage from "./_route.js";
 
 export default function add() {
   return (
     <>
-      <p className={style.title}>追加</p>
-      <p className={style.text}>新社員の情報を教えてください。</p>
+      <div className={style.container}>
+        <p className={style.title}>追加</p>
+        <p className={style.text}>新社員の情報を教えてください。</p>
 
-      <div className={style.inputform}>
+        <div className={style.inputform}>
+          <div>
+            <label htmlFor="staffName">社員の名前：</label>
+            <input
+              className={style.inputText}
+              type="text"
+              name="staffName"
+              id="staffName"
+              required
+            />
+          </div>
 
-      <div>
-        <label htmlFor="staffName">社員の名前：</label>
-        <input className={style.inputText} type="text" name="staffName" id="staffName" required />
+          <div>
+            <label htmlFor="department">部門：</label>
+            <input
+              className={style.inputText}
+              type="password"
+              name="department"
+              id="department"
+              required
+            />
+          </div>
+
+          <div>
+            <a href="addComplete">
+              <button type="submit" className={style.LoginSub}>
+                新社員登録
+              </button>
+            </a>
+          </div>
+
+          <div className={style.Cen}>
+            <a href="./">
+              <img src="../img/Cen2.png" width={100} />
+            </a>
+          </div>
+
+          <div className={style.footerPart}>
+            <h3 className="footerWord">システムの状況:X</h3>
+          </div>
+        </div>
       </div>
-
-      <div>
-        <label htmlFor="department">部門：</label>
-        <input className={style.inputText} type="password" name="department" id="department" required />
-      </div>
-      
-      <div><button type="submit" className={style.LoginSub}>新社員登録</button></div>
-
-      <div className={style.Cen}>
-        <a href="./menu.html"><img src="Cen2.png" width={100} /></a>
-      </div>
-
-      <div className={style.footerPart}>
-        <h3 className="footerWord">システムの状況:X</h3>
-      </div>
-
-    </div>
     </>
   );
 }
--- next-app/src/pages/addComplete.js
+++ next-app/src/pages/addComplete.js
@@ -1,29 +1,32 @@
-import style from "../styles/situation.module.css"
+import style from "../styles/situation.module.css";
+import movePage from "./_route.js";
 
 export default function addComplete() {
-    return (
-      <>
+  return (
+    <>
       <div className={style.container}>
         <div>
           <h1 className={style.title}>追加</h1>
         </div>
 
         <div className={style.inputform}>
-          <div  className={style.text}>
+          <div className={style.text}>
             <h2> 社員 : 社員のID追加できました</h2>
             <h2>ありがとうございました！</h2>
           </div>
 
           <div className={style.Cen}>
-              <a href="./completoin.js"><img src="./img/Cen2.png" width={100} /></a>
+            <a href="./completoin">
+              <img src="./img/Cen2.png" width={100} />
+            </a>
           </div>
 
           <div className={style.footerPart}>
             <h3 className="footerWord">システムの状況:X</h3>
           </div>
-        <hr className={style.hr}></hr>
+          <hr className={style.hr}></hr>
         </div>
-        </div>
-      </>
-    );
+      </div>
+    </>
+  );
 }
