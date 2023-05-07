9c8ea6fd665e29942863d851c060f926f44d9199
gongye16 <kinggongye@gmail.com>
2023-05-07 16:26:55 +0900

    社員追加ページを作成しました

--- next-app/src/pages/add.js
+++ next-app/src/pages/add.js
@@ -1,55 +1,50 @@
-import style from "../styles/situation.module.css";
+import style from "../styles/add.module.css";
 import movePage from "./_route.js";
 
 export default function add() {
   return (
-    <>
-      <div className={style.container}>
-        <p className={style.title}>追加</p>
-        <p className={style.text}>新社員の情報を教えてください。</p>
+    <div className={style.container}>
+      <p className={style.title}>追加</p>
+      <p className={style.text}>新社員の情報を教えてください。</p>
 
-        <div className={style.inputform}>
-          <div>
-            <label htmlFor="staffName">社員の名前：</label>
-            <input
-              className={style.inputText}
-              type="text"
-              name="staffName"
-              id="staffName"
-              required
-            />
-          </div>
+      <div className={style.inputform}>
+        <div>
+          <label htmlFor="staffName">社員の名前：</label>
+          <input
+            className={style.inputText}
+            type="text"
+            name="staffName"
+            id="staffName"
+            required
+          />
+        </div>
 
-          <div>
-            <label htmlFor="department">部門：</label>
-            <input
-              className={style.inputText}
-              type="password"
-              name="department"
-              id="department"
-              required
-            />
-          </div>
+        <div>
+          <label htmlFor="department">部　門　 ：</label>
+          <input
+            className={style.inputText}
+            type="password"
+            name="department"
+            id="department"
+            required
+          />
+        </div>
 
+        <div className={style.img}>
           <div>
-            <a href="addComplete">
-              <button type="submit" className={style.LoginSub}>
-                新社員登録
-              </button>
+            <a href="./completoin">
+              <img src="../img/Left.png" width="100px"></img>
             </a>
           </div>
-
-          <div className={style.Cen}>
-            <a href="./">
-              <img src="../img/Cen2.png" width={100} />
+          <h3>システムの状況</h3>
+          <div>
+            <a href="./addComplete">
+              <img src="../img/Right.png" width="100px"></img>
             </a>
-          </div>
-
-          <div className={style.footerPart}>
-            <h3 className="footerWord">システムの状況:X</h3>
           </div>
         </div>
       </div>
-    </>
+      <hr className={style.hr}></hr>
+    </div>
   );
 }
