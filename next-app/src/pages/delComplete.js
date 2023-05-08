448f65af779c0bf17f42a3456c0c3af8effebb1f
gongye16 <kinggongye@gmail.com>
2023-05-08 19:47:13 +0900

    削除の画面を作りました

--- next-app/src/pages/delComplete.js
+++ next-app/src/pages/delComplete.js
@@ -1,9 +1,9 @@
 import style from "../styles/situation.module.css";
-import movePage from './_route.js';
+import movePage from "./_route.js";
 
 const delComplete = () => {
-    return (
-      <>
+  return (
+    <>
       <div className={style.container}>
         <div>
           <h1 classNaem={style.title}>削除</h1>
@@ -13,15 +13,20 @@ const delComplete = () => {
           <div>
             <h2 className={style.text}>社員 : (社員のID) 削除しました</h2>
           </div>
-        
-          <div className={style.footerPart}>
-            <h3 className={style.footerWord}>システムの状況:X</h3>
+
+          <div className={style.Cen}>
+            <a href="./completoin">
+              <img src="./img/Cen2.png" width={100} />
+            </a>
           </div>
-          
+
+          <div className={style.footerPart}>
+            <h3 className="footerWord">システムの状況:X</h3>
+          </div>
         </div>
         <hr className={style.hr}></hr>
       </div>
-      </>
-    );
-}
+    </>
+  );
+};
 export default delComplete;
--- next-app/src/pages/delete.js
+++ next-app/src/pages/delete.js
@@ -1,2 +1,50 @@
-import style from "../styles/situation.module.css";
+import style from "../styles/delete.module.css";
 import movePage from "./_route.js";
+
+export default function add() {
+  return (
+    <div className={style.container}>
+      <p className={style.title}>削除</p>
+      <p className={style.text}>旧社員の情報を教えてください。</p>
+
+      <div className={style.inputform}>
+        <div>
+          <label htmlFor="staffName">社員の名前：</label>
+          <input
+            className={style.inputText}
+            type="text"
+            name="staffName"
+            id="staffName"
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="department">部　門　 ：</label>
+          <input
+            className={style.inputText}
+            type="text"
+            name="department"
+            id="department"
+            required
+          />
+        </div>
+
+        <div className={style.img}>
+          <div>
+            <a href="./completoin">
+              <img src="../img/Left.png" width="100px"></img>
+            </a>
+          </div>
+          <h3>システムの状況</h3>
+          <div>
+            <a href="./delComplete">
+              <img src="../img/Right.png" width="100px"></img>
+            </a>
+          </div>
+        </div>
+      </div>
+      <hr className={style.hr}></hr>
+    </div>
+  );
+}
