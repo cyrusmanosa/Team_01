1711a2b0c9c30a2b56f9d40459ce2427de483161
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2023-05-08 10:14:21 +0900

    Update Situation5

---
+++ next-app/src/pages/situation5.js
@@ -0,0 +1,44 @@
+import style from "../styles/situation.module.css";
+import movePage from "./_route.js";
+const situation5 = () => {
+  return (
+    <div className={style.container}>
+      <div>
+        <h1 className={style.title}>安否登録</h1>
+        <p className={style.text}>避難はできますか？</p>
+      </div>
+
+      <form action="" method="post">
+        <div>
+          <input
+            className={style.safe}
+            type="button"
+            value="はい"
+            onClick={movePage("completoin")}
+          ></input>
+        </div>
+        <div>
+          <input
+            className={style.dangerous}
+            type="button"
+            value="いいえ"
+            onClick={movePage("completoin")}
+          ></input>
+        </div>
+      </form>
+
+      <div className={style.img}>
+        <a href="./situation3">
+          <img src="../img/Left.png" width="100px"></img>
+        </a>
+        <h3>システムの状況</h3>
+        <a href="./completoin">
+          <img src="../img/Right.png" width="100px"></img>
+        </a>
+      </div>
+      <hr className={style.hr}></hr>
+    </div>
+  );
+};
+
+export default situation5;
